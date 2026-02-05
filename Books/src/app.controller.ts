df8944867926d4941c64d78bcd2918a9ee056786
AhmadMasood-dev <ahmadmasood.dev@gmail.com>
2026-02-05 12:58:04 +0500

    learn about provider services and make request alsp

--- Books/src/app.controller.ts
+++ Books/src/app.controller.ts
@@ -1,26 +1,28 @@
 import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { BookService } from './book/book.service';
 
 @Controller('/api/v1/book')
 export class BookController {
+  constructor(private bookServices: BookService) {}
   @Post()
   addBook() {
-    return 'Book added successfully';
+    return this.bookServices.createBook();
   }
   @Delete('/delete')
   deleteBook() {
-    return 'Book deleted successfully';
+    return this.bookServices.deleteBook();
   }
   @Put('/update')
   updateBook() {
-    return 'Book updated successfully';
+    return this.bookServices.updateBook();
   }
   @Get()
   getBook() {
-    return 'Book details';
+    return this.bookServices.getBookDetails();
   }
   @Get('/:bookId')
   getBookById(@Param() params): string {
     console.log(params);
-    return `Book details for id ${params.bookId}`;
+    return this.bookServices.getBookById(params.bookId);
   }
 }
--- Books/src/root.module.ts
+++ Books/src/root.module.ts
@@ -3,11 +3,12 @@ import { ChatModule } from './chat/chat.module';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
 import { BookController } from './app.controller';
+import { BookService } from './book/book.service';
 
 @Module({
   imports: [ChatModule, OrdersModule, UsersModule],
   controllers: [BookController],
-  providers: [],
+  providers: [BookService],
   exports: [],
 })
 export class RootModule {
